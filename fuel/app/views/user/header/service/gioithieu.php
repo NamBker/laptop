67e92d185a0fbb793e884ad0e6ce5150a680d8c7
nambker <namnv@runsystem.net>
2017-01-04 15:28:49 +0700

    contact-store

--- fuel/app/views/user/header/service/gioithieu.php
+++ fuel/app/views/user/header/service/gioithieu.php
@@ -3,13 +3,6 @@
       	<div class="container">
       		<div class="row">
       			<div class="col-md-4">
-      				<div class="single-sidebar">
-      					<h2 class="sidebar-title">Search Products</h2>
-      					<form action="#">
-      						<input type="text" placeholder="Search products...">
-      						<input type="submit" value="Search">
-      					</form>
-      				</div>
               <div class="single-sidebar">
                 <h2 class="sidebar-title">Products</h2>
                 <div class="thubmnail-recent">
@@ -40,16 +33,6 @@
                     <ins>$700.00</ins> <del>$800.00</del>
                   </div>                             
                 </div>
-              </div>
-              <div class="single-sidebar">
-               <h2 class="sidebar-title">Recent Posts</h2>
-               <ul>
-                <li><a href="#">Sony Smart TV - 2015</a></li>
-                <li><a href="#">Sony Smart TV - 2015</a></li>
-                <li><a href="#">Sony Smart TV - 2015</a></li>
-                <li><a href="#">Sony Smart TV - 2015</a></li>
-                <li><a href="#">Sony Smart TV - 2015</a></li>
-              </ul>
               </div>
             </div>
 
